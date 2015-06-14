93c29dcc4caecda266c3babad616c3f3aa666c5f
Dave <maligree@gmx.de>
2015-06-14 16:54:31 +0100

    Added some config documentation.

--- config_clean.py
+++ config_clean.py
@@ -1,17 +1,40 @@
+# The API Keys and oauth tokens from https://apps.twitter.com/
+# 
+# Create a new App, set the Access Level to Read/Write, generate Tokens, copy and paste them here:
 app_key=""
 app_secret=""
 oauth_token=""
 oauth_token_secret=""
 
-owner=""
+# The owner of the bot. Should be a real user who actually reads his timeline.
+# Do not prepend the @ mark.
+owner="anathem"
 
+# What search query to track.
+# This should be a search query understood by both search API and streaming API.
+# Hint: "-filter:retweets" won't work in streaming API, which is why it's automatically appended
+# for the catch-up search when starting up.
+track = '#Fotoprojekt2015'
+
+# Enter a local directory for the archive.
+# If it does not exist yet, it is attempted to be created.
+archive_dir = "archive"
+
+# The list of approved twitter users with additional rights.
 approved_list = { 
   'name': 'Approved',
   'owner': owner,
 }
 
-retweet_rate_limit_per_hour = 5
+# Rate limit per 15min sliding window.
+# Never enter more than 15 here or you might run the risk that Twitter blocks you for hammering!
+rate_limit_per_15min = 12
 
-track = ''
+# Whether to actually tweet anything.
+# If this is True, this Bot only archives tweets.
+dry_run = False
 
-dry_run = True
+# How many Tweets to read back on startup/restart?
+# This is limited to 100 by the Twitter API. Also, don't count on getting that many.
+# Don't set this to less than 5.
+read_back = 100
